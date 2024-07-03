cf8d75f9095f6813a54eb65aabde6efee83d41fb
Jun, Jakub <jakubjun21@gmail.com>
2024-07-03 20:32:34 +0200

    add custom error struct

--- src/main.rs
+++ src/main.rs
@@ -9,47 +9,66 @@ enum BencodeType {
 }
 
 #[derive(Debug)]
+struct BencodeError {
+    message: String,
+}
+
+impl BencodeError {
+    fn from_str(msg: &str) -> Self {
+        BencodeError {
+            message: String::from(msg),
+        }
+    }
+}
+
+#[derive(Debug)]
 struct Bencode {
     node: BencodeType,
     len: usize,
 }
 
 impl Bencode {
-    fn from_str(bencoded_input: &str) -> Self {
+    fn from_str(bencoded_input: &str) -> Result<Self, BencodeError> {
         let first_char = bencoded_input.chars().nth(0).unwrap();
         match first_char {
             'i' => Bencode::parse_int(bencoded_input),
             'l' => Bencode::parse_list(bencoded_input),
             'd' => Bencode::parse_dict(bencoded_input),
             _ if first_char.is_numeric() => Bencode::parse_str(bencoded_input),
-            _ => panic!("unsupported bencode type char"),
+            _ => Err(BencodeError::from_str("unsupported bencode type char")),
         }
     }
 
-    fn parse_int(bencoded_str: &str) -> Bencode {
-        let mut parts = bencoded_str[1..].split("e");
+    fn from_file(path: &str) -> Result<Self, BencodeError> {
+        todo!()
+    }
+
+    fn parse_int(bencoded_str: &str) -> Result<Self, BencodeError> {
+        let mut parts = bencoded_str[1..].split('e');
         let part = parts.next().unwrap();
         let len = part.len() + 2;
-        let int = part.parse::<i32>().unwrap();
 
-        Bencode {
-            node: BencodeType::Int(int),
-            len,
+        match part.parse::<i32>() {
+            Ok(int) => Ok(Bencode {
+                node: BencodeType::Int(int),
+                len,
+            }),
+            Err(_) => Err(BencodeError::from_str("...")),
         }
     }
 
-    fn parse_str(bencoded_str: &str) -> Bencode {
-        let first_part = bencoded_str.split(":").next().unwrap();
+    fn parse_str(bencoded_str: &str) -> Result<Self, BencodeError> {
+        let first_part = bencoded_str.split(':').next().unwrap();
         let len_first_part = first_part.len();
         let len_second_part = first_part.parse::<usize>().unwrap();
         let str = &bencoded_str[len_first_part + 1..len_first_part + 1 + len_second_part];
-        Bencode {
+        Ok(Bencode {
             node: BencodeType::Str(str.as_bytes().into()),
             len: len_first_part + 1 + len_second_part,
-        }
+        })
     }
 
-    fn parse_list(bencoded_str: &str) -> Bencode {
+    fn parse_list(bencoded_str: &str) -> Result<Self, BencodeError> {
         let mut chars = bencoded_str.chars();
         let mut result: Vec<BencodeType> = vec![];
 
@@ -62,19 +81,19 @@ impl Bencode {
             if ch == 'e' {
                 break;
             }
-            let b = Bencode::from_str(remaining_bencoded_str);
+            let b = Bencode::from_str(remaining_bencoded_str)?;
             remaining_bencoded_str = &remaining_bencoded_str[b.len..];
             result.push(b.node);
             chars.nth(b.len);
         }
 
-        Bencode {
+        Ok(Bencode {
             node: BencodeType::List(result),
             len: bencoded_str.len() - remaining_bencoded_str.len() + 1,
-        }
+        })
     }
 
-    fn parse_dict(bencoded_str: &str) -> Bencode {
+    fn parse_dict(bencoded_str: &str) -> Result<Self, BencodeError> {
         let mut chars = bencoded_str.chars();
         let mut result: HashMap<String, BencodeType> = HashMap::new();
 
@@ -88,10 +107,10 @@ impl Bencode {
                 break;
             }
 
-            let key = Bencode::from_str(remaining_bencoded_str);
+            let key = Bencode::from_str(remaining_bencoded_str)?;
             remaining_bencoded_str = &remaining_bencoded_str[key.len..];
 
-            let val = Bencode::from_str(remaining_bencoded_str);
+            let val = Bencode::from_str(remaining_bencoded_str)?;
             remaining_bencoded_str = &remaining_bencoded_str[val.len..];
 
             if let BencodeType::Str(str) = key.node {
@@ -100,11 +119,11 @@ impl Bencode {
             chars.nth(key.len + val.len);
         }
 
-        Bencode {
+        Ok(Bencode {
             node: BencodeType::Dict(result),
             // 1 is the ':' delimiter
             len: bencoded_str.len() - remaining_bencoded_str.len() + 1,
-        }
+        })
     }
 }
 
@@ -138,7 +157,7 @@ mod test {
     use super::*;
     #[test]
     fn test_int() {
-        let b = Bencode::from_str("i42e");
+        let b = Bencode::from_str("i42e").unwrap();
         let n = b.node;
         assert!(matches!(n, BencodeType::Int(_)));
         if let BencodeType::Int(int) = n {
@@ -148,7 +167,7 @@ mod test {
     }
     #[test]
     fn test_str() {
-        let b = Bencode::from_str("10:spam1spam1");
+        let b = Bencode::from_str("10:spam1spam1").unwrap();
         let n = b.node;
         assert!(matches!(n, BencodeType::Str(_)));
         if let BencodeType::Str(str) = n {
@@ -158,7 +177,7 @@ mod test {
     }
     #[test]
     fn test_list() {
-        let b = Bencode::from_str("li42ei15e5:abcdee");
+        let b = Bencode::from_str("li42ei15e5:abcdee").unwrap();
         let n = b.node;
         assert!(matches!(n, BencodeType::List(_)));
         assert_eq!(b.len, 17);
@@ -195,23 +214,34 @@ mod test {
     }
     #[test]
     fn test_dict() {
-        let b = Bencode::from_str("d3:bar4:spam3:fooi42ee");
+        let b = Bencode::from_str("d3:bar4:spam3:fooi42ee").unwrap();
         let n = b.node;
 
         assert!(matches!(n, BencodeType::Dict(_)));
         assert_eq!(b.len, 22);
-        if let BencodeType::Dict(dict) = n {
-            let bar = dict.get("bar").unwrap();
-            let foo = dict.get("foo").unwrap();
+        let dict = if let BencodeType::Dict(dict) = n {
+            dict
+        } else {
+            panic!()
+        };
 
-            if let BencodeType::Str(str) = bar {
-                assert_eq!(
-                    String::from_utf8(str.clone()).unwrap(),
-                    String::from("spam")
-                );
-            } else {
-                panic!()
-            }
+        let bar = dict.get("bar").unwrap();
+
+        if let BencodeType::Str(str) = bar {
+            assert_eq!(
+                String::from_utf8(str.clone()).unwrap(),
+                String::from("spam")
+            );
+        } else {
+            panic!()
+        }
+
+        let foo = dict.get("foo").unwrap();
+
+        if let BencodeType::Int(int) = foo {
+            assert_eq!(int, &42);
+        } else {
+            panic!()
         }
     }
 }
